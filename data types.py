e18d298becc7d49a983feed6f55ee2c8379b4e86
Manisiva3 <99855737+Manisiva3@users.noreply.github.com>
2023-11-15 15:49:40 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,9 @@
+"AIM:write a program to demonstrate different number data types in python"
+
+
+a=10;
+b=11.5;
+c=2.05j;
+print("a is Type of",type(a));
+print("b is Type of",type(b));
+print("c is Type of",type(c));
---
+++ string.py
@@ -0,0 +1,6 @@
+s1=input("Enter first String:");
+s2=input("Enter Second String:");
+print("First string is:",s1);
+print("second string is:",s2);
+print("concatenations of two strings:",s1+s2);
+print("substring of given string:",s1[1:4]);
