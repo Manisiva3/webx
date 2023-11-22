614a858348c76f8effcb699d3d266357f8d431ff
Manisiva3 <99855737+Manisiva3@users.noreply.github.com>
2023-11-22 15:02:03 +0530

    Add files via upload

---
+++ exam2.py
@@ -0,0 +1,8 @@
+L1=[500,800,600,200,900]
+start=1
+sum=0
+for c in range(start,4):
+    sum=sum+L1[c]
+    print(c,":",sum)
+    sum=sum+L1[0]*10
+
---
+++ siva.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create a one-dimensional array
+one_dimensional_array = np.array([1, 2, 3, 4, 5])
+
+print(one_dimensional_array)
+
+import numpy as np
+
+# Create a two-dimensional array
+two_dimensional_array = np.array([[1, 2, 3],
+                                 [4, 5, 6],
+                                 [7, 8, 9]])
+
+print(two_dimensional_array)
